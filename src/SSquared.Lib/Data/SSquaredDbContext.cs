c9ff3113cbbc16a2d17a55119fe1bcd1254480af
Brandon Matheson <Cityonhill93@gmail.com>
2023-11-09 16:12:26 -0600

    Addd a DB context

---
+++ src/SSquared.Lib/Data/SSquaredDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SSquared.Lib.Data
+{
+    public class SSquaredDbContextDbContext : DbContext
+    {
+
+        public SSquaredDbContextDbContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "SSquared.db");
+        }
+
+        public string DbPath { get; }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
