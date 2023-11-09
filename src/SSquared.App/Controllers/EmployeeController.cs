42ea5302f3566f7403ab364ff73e50c570f969eb
Brandon Matheson <Cityonhill93@gmail.com>
2023-11-09 16:45:56 -0600

    Stub out non-implemented API controllers

---
+++ src/SSquared.App/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SSquared.App.Controllers
+{
+    [Route("api/Employees")]
+    public class EmployeeController
+    {
+        [HttpGet("{id}")]
+        public Task<IActionResult> GetEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("")]
+        public Task<IActionResult> GetEmployees()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
