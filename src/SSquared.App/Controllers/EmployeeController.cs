8eaab100d6ff0252df3bea819bd5d927e5adec6e
Brandon Matheson <Cityonhill93@gmail.com>
2023-11-09 16:55:11 -0600

    Add API versioning

--- src/SSquared.App/Controllers/EmployeeController.cs
+++ src/SSquared.App/Controllers/EmployeeController.cs
@@ -1,17 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SSquared.App.Controllers
 {
-    [Route("api/Employees")]
+    [ApiController]
+    [Route("api/{v:apiVersion}/Employees")]
     public class EmployeeController
     {
         [HttpGet("{id}")]
+        [ApiVersion("1")]
         public Task<IActionResult> GetEmployee(int id)
         {
             throw new NotImplementedException();
         }
 
         [HttpGet("")]
+        [ApiVersion("1")]
         public Task<IActionResult> GetEmployees()
         {
             throw new NotImplementedException();
--- src/SSquared.App/Program.cs
+++ src/SSquared.App/Program.cs
@@ -1,3 +1,4 @@
+using Asp.Versioning;
 using Microsoft.EntityFrameworkCore;
 using SSquared.Lib.Data;
 
@@ -6,6 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<SSquaredDbContextDbContext>();
+builder.Services.AddApiVersioning(e =>
+{
+    e.DefaultApiVersion = new ApiVersion(1);
+    e.AssumeDefaultVersionWhenUnspecified = true;
+    e.ReportApiVersions = true;
+});
 
 var app = builder.Build();
 
