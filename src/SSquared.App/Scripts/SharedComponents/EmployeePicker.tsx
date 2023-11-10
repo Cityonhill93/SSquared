ba04efb2b01f45893740eccd1e946ebb7f843733
Brandon Matheson <Cityonhill93@gmail.com>
2023-11-09 21:20:40 -0600

    Stub out an employee picker component

--- src/SSquared.App/Scripts/SharedComponents/EmployeePicker.tsx
+++ src/SSquared.App/Scripts/SharedComponents/EmployeePicker.tsx
@@ -1,1 +1,54 @@
-﻿
+﻿import * as React from 'react'
+import { IEmployeeDto } from '../DTO/IEmployeeDto';
+import { getEmployees } from '../DataAccess/EmployeeDataAccess';
+
+export class EmployeePicker extends React.Component<EmployeePickerProps, EmployeePickerState> {
+    constructor(p: EmployeePickerProps) {
+        super(p);
+
+        this.getState = this.getState.bind(this);
+    }
+
+    public render() {
+        var state = this.getState();
+        if (!state.loaded) {
+            return null;
+        }
+
+        var items = state.items.map(i => <option value={i.id}>{i.firstName} { i.lastName}</option>);
+        return <select className="form-select">
+            { items}
+        </select>
+    }
+
+    async componentDidMount() {
+        var state = this.getState();
+        if (!state.loaded) {
+            var employees = await getEmployees();
+            state.loaded = true;
+            state.items = employees;
+            this.setState(state);
+        }
+    }
+
+    getState(): EmployeePickerState {
+        var state = this.state;
+        if (!state) {
+            state = {
+                items: [],
+                loaded: false
+            };
+        }
+
+        return state;
+    }
+}
+
+export class EmployeePickerProps {
+
+}
+
+class EmployeePickerState {
+    items: IEmployeeDto[];
+    loaded: boolean;
+}
