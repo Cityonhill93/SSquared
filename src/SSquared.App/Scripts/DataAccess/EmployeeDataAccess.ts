9085cb38ee4cc311417a734af0e15927d221016b
Brandon Matheson <Cityonhill93@gmail.com>
2023-11-09 21:00:17 -0600

    Start to add JS data access files

---
+++ src/SSquared.App/Scripts/DataAccess/EmployeeDataAccess.ts
@@ -0,0 +1,19 @@
+﻿import { IEmployeeDto } from "../DTO/IEmployeeDto";
+import { IExpandedEmployeeDto } from "../DTO/IExpandedEmployeeDto";
+import $ from "jquery";
+
+const baseApiPath = "/api/1/Employees";
+
+export async function getEmployee(id:number): Promise<IExpandedEmployeeDto> {
+    var url = `${baseApiPath}/${id}`;
+    return $.get(url);
+}
+
+export async function getEmployees(filter: string | undefined = null): Promise<IEmployeeDto[]> {
+    var url = baseApiPath;
+    if (filter && filter.length > 0) {
+        url += `?filter=${filter}`;
+    }
+
+    return $.get(url);
+}
