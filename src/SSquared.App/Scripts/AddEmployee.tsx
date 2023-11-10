c8f6fe81f2964a834ae13f1303787216c7171c21
Brandon Matheson <Cityonhill93@gmail.com>
2023-11-10 15:55:49 -0600

    First pass at the add employee JS

--- src/SSquared.App/Scripts/AddEmployee.tsx
+++ src/SSquared.App/Scripts/AddEmployee.tsx
@@ -1,3 +1,55 @@
-﻿export function init() {
+﻿import React from "react";
+import { addEmployee } from "./DataAccess/EmployeeDataAccess";
+import { IAddEmployeeDto } from "./DTO/IAddEmployeeDto";
+import { EmployeeEditor } from "./SharedComponents/EmployeeEditor";
+import { IExpandedEmployeeDto } from "./DTO/IExpandedEmployeeDto";
+import ReactDOM from "react-dom";
+
+export function init() {
+    var element = document.getElementsByClassName("react-wrapper")[0];
+    ReactDOM.render(<AddEmployeePage></AddEmployeePage>, element);
+}
+
+class AddEmployeePage extends React.Component<AddEmployeePageProps, AddEmployeePageState> {
+    constructor(p: AddEmployeePageProps) {
+        super(p);
+
+        this.saveEmployee = this.saveEmployee.bind(this);
+    }
+
+    public render() {
+        var employee: IExpandedEmployeeDto = {
+            id:0,
+            firstName: "",
+            lastName: "",
+            employeeId: "",
+            getUrl: "",
+            employees: [],
+            manager:null
+        };
+
+        return <EmployeeEditor employee={employee} key="employeeEditor" onSave={ this.saveEmployee} ></EmployeeEditor>
+    }
+
+    async saveEmployee(firstName: string, lastName: string, employeeId: string, managerId: number | null): Promise<void> {
+        var dto: IAddEmployeeDto = {
+            firstName: firstName,
+            lastName: lastName,
+            employeeId: employeeId,
+            managerId:managerId
+        };
+
+        await addEmployee(dto);
+
+        alert("Employee created!");
+        window.location.href = "./";
+    }
+}
+
+class AddEmployeePageProps {
+
+}
+
+class AddEmployeePageState {
 
 }
