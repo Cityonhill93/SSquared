2a3f6ba7a5b496ae863494e00c05237471ebd7cd
Brandon Matheson <Cityonhill93@gmail.com>
2023-11-10 19:49:53 -0600

    Initial version of a role picker component

---
+++ src/SSquared.App/Scripts/SharedComponents/RolePicker.tsx
@@ -0,0 +1,115 @@
+﻿
+import React, { ChangeEvent } from "react";
+import { IRoleDto } from "../DTO/IRoleDto";
+import { getRoles } from "../DataAccess/RoleDataAccess";
+
+export class RolePicker extends React.Component<RolePickerProps, RolePickerState> {
+    constructor(p: RolePickerProps) {
+        super(p);
+
+        this.getState = this.getState.bind(this);
+        this.itemToggled = this.itemToggled.bind(this);
+    }
+
+    public render() {
+        var state = this.getState();
+        var items = state.allRoles.map(role => <RolePickerItem role={role} roleToggled={this.itemToggled} selected={state.selectedRoleIds.includes(role.id)} ></RolePickerItem>);
+
+        return <div>
+            {items}
+        </div>;
+        
+    }
+
+    async componentDidMount() {
+        var state = this.getState();
+        state.allRoles = await getRoles();
+        this.setState(state);
+    }
+
+    getState(): RolePickerState {
+        var state = this.state;
+        if (!state) {
+            state = {
+                allRoles: [],
+                selectedRoleIds:[]
+            };
+        }
+
+        return state;
+    }
+
+    itemToggled(roleId: number, selected: boolean) {
+        var state = this.getState();
+
+        if (selected && !state.selectedRoleIds.includes(roleId)) {
+            state.selectedRoleIds.push(roleId);
+            this.setState(state);
+        }
+        else if (!selected && state.selectedRoleIds.includes(roleId)) {
+            var index = state.selectedRoleIds.indexOf(roleId);
+            state.selectedRoleIds.splice(index, 1);
+            this.setState(state);
+        }
+
+        this.props.itemsChanged(state.selectedRoleIds);
+    }
+}
+
+export class RolePickerProps {
+    itemsChanged: (selectedRoleIds: number[]) => void;
+}
+
+class RolePickerState {
+    allRoles: IRoleDto[];
+    selectedRoleIds: number[];
+}
+
+class RolePickerItem extends React.Component<RolePickerItemProps, RolePickerItemState> {
+    constructor(p: RolePickerItemProps) {
+        super(p);
+    }
+
+    public render() {
+        var role = this.props.role;
+        var checked = this.props.selected;
+
+        var checkboxElement = (checked
+            ? <input type="checkbox" value={role.id} checked ></input>
+            : <input type="checkbox" value={role.id}></input>);
+
+        return <label>
+            {checkboxElement}
+            { role.name}
+        </label>;
+    }
+
+    getState(): RolePickerItemState {
+        var state = this.state;
+        if (!state) {
+            state = {
+                selected:this.props.selected
+            };
+        }
+
+        return state;
+    }
+
+    roleToggled(e:ChangeEvent) {
+        var element = $(e.target);
+        var roleId = this.props.role.id;
+        var selected = element.is(":checked");
+
+        this.props.roleToggled(roleId, selected);
+    }
+}
+
+class RolePickerItemProps {
+    role: IRoleDto;
+    roleToggled: (roleId: number, selected: boolean) => void;
+    selected:boolean
+}
+
+class RolePickerItemState {
+    selected: boolean;
+}
