0362980abafdcb5be978754c78b41b495d72ec5d
Brandon Matheson <Cityonhill93@gmail.com>
2023-11-11 13:24:36 -0600

    Use a switch to find the right bootstrap class name to use for a node

--- src/SSquared.App/Scripts/OrgChart.tsx
+++ src/SSquared.App/Scripts/OrgChart.tsx
@@ -13,9 +13,16 @@ export async function init(employeeId: number) {
 }
 
 class OrgChartNodeComponent extends React.Component<OrgChartNodeComponentProps, OrgChartNodeComponentState>{
+    constructor(p: OrgChartNodeComponentProps) {
+        super(p);
+
+        this.getBootstrapClassName = this.getBootstrapClassName.bind(this);
+    }
+
     public render() {
         var node = this.props.node;
-        var children = node.nodes.map(subNode => <OrgChartNodeComponent key={`subNode${subNode.id}`} isChild={true} node={subNode}></OrgChartNodeComponent>);
+        var nodeLevel = this.props.nodeLevel ?? 0;
+        var children = node.nodes.map(subNode => <OrgChartNodeComponent key={`subNode${subNode.id}`} node={subNode} nodeLevel={nodeLevel+1} ></OrgChartNodeComponent>);
         var childContainer = (children.length > 0
             ?   <div className="container org-chart-node-children">
                     <div className="row">
@@ -23,19 +30,36 @@ class OrgChartNodeComponent extends React.Component<OrgChartNodeComponentProps, 
                     </div>
                 </div>  
             : null);
+
         
-        return <div className={`org-chart-node ${(this.props.isChild ? "col-6" : "")}`} >
+
+        return <div className={`org-chart-node ${this.getBootstrapClassName()}`} >
             <div className="org-chart-node-details">
                 <span>{node.firstName} {node.lastName}</span>
             </div>
             {childContainer}          
         </div>;
     }
+
+    getBootstrapClassName(): string {
+        var nodeLevel = this.props.nodeLevel;
+
+        switch (nodeLevel) {
+            case 0:
+                return "";
+            case 1:
+                return "col-4";
+            case 2:
+                return "col-6";
+            default:
+                return "col-12";
+        }
+    }
 }
 
 class OrgChartNodeComponentProps {
-    isChild?: boolean;
-    node:IOrgChartNode
+    node: IOrgChartNode
+    nodeLevel?: number;
 } 
 
 class OrgChartNodeComponentState {
